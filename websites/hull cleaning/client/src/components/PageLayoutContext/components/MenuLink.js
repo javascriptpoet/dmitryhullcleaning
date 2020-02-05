691b02b20a72682ea0e6b14393ddf1a3ff7a4279
jspoet <skihappy17@gmail.com>
2020-02-04 21:48:56 -0800

    initial

--- websites/hull cleaning/client/src/components/PageLayoutContext/components/MenuLink.js
+++ websites/hull cleaning/client/src/components/PageLayoutContext/components/MenuLink.js
@@ -10,38 +10,38 @@ const MenuLink = ({ to, label, side = "left", customCss = css`` }) => {
 
   return (
     <li
-      css={css`
-        width: 100%;
-        margin: 0;
-        padding: 0;
-      `}
+      css={[
+        customCss,
+        css`
+          width: 100%;
+          margin: 0;
+          padding: 0;
+        `
+      ]}
       key={label}
     >
       <Link
         to={to || "/"}
-        css={[
-          customCss,
-          css`
-            display: block;
-            text-decoration: none;
-            line-height: 70px;
-            height: 60px;
-            color: #000;
-            font-size: 20px;
-            border-bottom: 8px solid transparent;
+        css={css`
+          display: block;
+          text-decoration: none;
+          line-height: 70px;
+          height: 60px;
+          color: #000;
+          font-size: 20px;
+          border-bottom: 8px solid transparent;
 
-            &[link-type="current"] {
-              border-bottom-color: #000;
-              &:hover {
-                cursor: default;
-              }
+          &[link-type="current"] {
+            border-bottom-color: #000;
+            &:hover {
+              cursor: default;
             }
+          }
 
-            &[link-type="default"]:hover {
-              color: ${colors.green};
-            }
-          `
-        ]}
+          &[link-type="default"]:hover {
+            color: ${colors.green};
+          }
+        `}
         link-type="default"
         getProps={({ isCurrent }) =>
           isCurrent ? { "link-type": "current" } : {}
